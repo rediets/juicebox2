0724c6445d7921bdf15b97451451e45cec7cb28b
sibahtu <redietsibahtu@gmail.com>
2023-05-18 12:39:51 -0500

    server first part

---
+++ api/users.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const { getAllUsers } = require('../db');
+const usersRouter = express.Router();
+
+
+usersRouter.use((req, res, next) => {
+    console.log("A requet is being made to /users");
+
+    next();
+});
+
+
+usersRouter.get('/', async (req, res,) => {
+      const users = await getAllUsers();
+
+    res.send({
+        users
+    });
+});
+
+
+module.exports = usersRouter;
